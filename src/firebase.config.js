3169472e3b164a1f8115bd3f74e26c8e396ca7b1
Shivam Mundhra <smundhra4478@gmail.com>
2020-04-14 22:57:22 +0530

    Firebase config added

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBxR9ox1-TYRvlSaSDgTBRQAuOPhVvzeT0",
+  authDomain: "crown-clothing-5f5a6.firebaseapp.com",
+  databaseURL: "https://crown-clothing-5f5a6.firebaseio.com",
+  projectId: "crown-clothing-5f5a6",
+  storageBucket: "crown-clothing-5f5a6.appspot.com",
+  messagingSenderId: "358317209213",
+  appId: "1:358317209213:web:599674560906ecd7982314",
+  measurementId: "G-BSYRKCFSD8",
+};
